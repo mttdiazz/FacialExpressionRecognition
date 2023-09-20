e6393cccc118f6b9cdf6cc1bc9268e8fd4b2a98e
Mateo <mateomacagnidiaz@gmail.com>
2023-09-19 21:45:58 -0300

    Revert "Architecture Updated"
    This reverts commit 536b70511f5dd1d8a0826f356d65562a2bf90e73.

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,10 +1,5 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
-import numpy as np
-import cv2
-from tensorflow.keras.preprocessing import image
 
 # Define the paths to your training and testing data directories
 trainingDataDir = '/Users/mateo/Documents/Mateo/FacialExpressionRecognition/FacialExpression/train'
@@ -50,7 +45,6 @@ validation_generator = train_datagen.flow_from_directory(
     shuffle=True
 )
 
-<<<<<<< Updated upstream
 # Define the model architecture
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(img_width, img_height, 1)),
@@ -72,43 +66,11 @@ model = tf.keras.models.Sequential([
 
 
 # Compile the model with a custom learning rate
-=======
-# Define the upgraded model architecture
-model = Sequential([
-    Conv2D(64, (3, 3), activation='relu', input_shape=(img_width, img_height, 1)),
-    BatchNormalization(),
-    Conv2D(64, (3, 3), activation='relu'),
-    BatchNormalization(),
-    MaxPooling2D(2, 2),
-    Dropout(0.25),
-
-    Conv2D(128, (3, 3), activation='relu'),
-    BatchNormalization(),
-    Conv2D(128, (3, 3), activation='relu'),
-    BatchNormalization(),
-    MaxPooling2D(2, 2),
-    Dropout(0.25),
-
-    Conv2D(256, (3, 3), activation='relu'),
-    BatchNormalization(),
-    Conv2D(256, (3, 3), activation='relu'),
-    BatchNormalization(),
-    MaxPooling2D(2, 2),
-    Dropout(0.25),
-
-    Flatten(),
-    Dense(512, activation='relu'),
-    BatchNormalization(),
-    Dropout(0.5),
-    Dense(7, activation='softmax')
-])
-
-# Compile the upgraded model
->>>>>>> Stashed changes
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
 model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train the upgraded model
+
+# Train the model
 model.fit(
     train_generator,
     steps_per_epoch=train_generator.samples // batch_size,
@@ -117,10 +79,10 @@ model.fit(
     validation_steps=validation_generator.samples // batch_size
 )
 
-# Save the upgraded model
-model.save('FacialExpressionModel_Upgraded.h5')
+# Save the model
+model.save('FacialExpressionModel.h5')
 
-# Evaluate the upgraded model on the testing data
+# Evaluate the model on the testing data
 test_generator = test_datagen.flow_from_directory(
     testingDataDir,
     target_size=(img_width, img_height),
